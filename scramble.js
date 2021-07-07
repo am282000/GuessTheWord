44e13d5fa3d3f2f37ec69168ff06b746ce7bdd23
Ashish Madan <66418119+am282000@users.noreply.github.com>
2021-07-08 00:55:36 +0530

    Update Words for children

--- scramble.js
+++ scramble.js
@@ -4,7 +4,46 @@ const btn =document.querySelector('.btn');
 let play=false;
 let newWords="";
 let randWords="";
-let sWords =['python','java','javascript','react','angular','android','programmer','developer','microsoft','ant',	'bear',	'bee',	'bird',	'cat',	'chicken',	'cow',	'dog',	'elephant',	'fish',	'fox',	'frog',	'giraffe',	'horse',	'lamb',	'panda',	'penguin',	'pig',	'rabbit',	'seahorse',	'shark',	'sheep',	'snail',	'snake',	'spider',	'squirrel',	'toucan',	'turkey',	'turtle',	'whale',	'zebra',	'apple',	'avocado',	'banana',	'bell pepper',	'blackberry',	'blueberry',	'cabbage',	'carrot',	'cherry',	'corn',	'cucumber',	'eggplant',	'garlic',	'grapes',	'kiwi',	'lemon',	'melon',	'mushroom',	'onion',	'orange',	'peach',	'pear',	'pineapple',	'plum',	'raspberry',	'strawberry',	'tomato',	'watermelon',	'zucchini',	'bathtub',	'bed',	'broom',	'computer',	'cup',	'door',	'fork',	'fridge',	'frying pan',	'glass',	'mixer',	'hanger',	'iron',	'knife',	'ladle',	'bulb',	'microwave',	'mirror',	'oven mitt',	'pan',	'plate',	'sink',	'sofa',	'spoon',	'table',	'tap',	'toaster',	'toilet',	'trash can',	'wardrobe',	'window',	'acorn',	'autumn',	'bug',	'butterfly',	'cactus',	'cloud',	'feather',	'fir tree',	'flower',	'grass',	'holly',	'leaf',	'mountain',	'palm tree',	'rain',	'rainbow',	'reed',	'rose',	'spring',	'stone',	'summer',	'tree',	'volcano',	'winter',	'black',	'blue',	'brown',	'green',	'orange',	'pink',	'purple',	'red',	'white',	'yellow',	'bag',	'belt',	'boot',	'cap',	'dress',	'hat',	'heels',	'jeans',	'tie',	'panties',	'pants',	'shirt',	'shoes',	'sneakers',	'socks',	'suit',	'sweater',	'tee shirt',	'thongs',	'arm',	'bandage',	'blood',	'brain',	'ear',	'embryo',	'eye',	'foot',	'hand',	'heart',	'kidney',	'knee',	'lungs',	'mouth',	'pills',	'syringe',	'skeleton',	'skull',	'teeth',	'thermometer',	'thumb',	'badminton',	'baseball',	'basketball',	'biking',	'bowling',	'boxing',	'cricket',	'golf',	'ice hockey',	'jogging',	'soccer',	'swimming',	'tennis',	'whistle',	'surfing',	'windsurfing',	'ant',	'bear',	'bee',	'bird',	'cat',	'chicken',	'cow',	'dog',	'elephant',	'fish',	'fox',	'frog',	'giraffe',	'horse',	'lamb',	'panda',	'penguin',	'pig',	'rabbit',	'seahorse',	'shark',	'sheep',	'snail',	'snake',	'spider',	'squirrel',	'toucan',	'turkey',	'turtle',	'whale',	'zebra',	'apple',	'avocado',	'banana',	'bell pepper',	'blackberry',	'blueberry',	'cabbage',	'carrot',	'cherry',	'corn',	'cucumber',	'eggplant',	'garlic',	'grapes',	'kiwi',	'lemon',	'melon',	'mushroom',	'onion',	'orange',	'peach',	'pear',	'pineapple',	'plum',	'raspberry',	'strawberry',	'tomato',	'watermelon',	'zucchini',	'bathtub',	'bed',	'broom',	'computer',	'cup',	'door',	'fork',	'fridge',	'frying pan',	'glass',	'mixer',	'hanger',	'iron',	'knife',	'ladle',	'bulb',	'microwave',	'mirror',	'oven mitt',	'pan',	'plate',	'sink',	'sofa',	'spoon',	'table',	'tap',	'toaster',	'toilet',	'trash can',	'wardrobe',	'window',	'badminton',	'baseball',	'basketball',	'biking',	'bowling',	'boxing',	'cricket',	'golf',	'ice hockey',	'jogging',	'soccer',	'swimming',	'tennis',	'whistle',	'surfing',	'windsurfing']; 
+let sWords =['ant', 'bear', 'bee', 'bird', 'cat', 'chicken', 'cow', 'dog', 'elephant',
+			'fish', 'fox', 'frog', 'giraffe', 'horse', 'lamb', 'panda', 'penguin',
+			'pig', 'rabbit', 'seahorse', 'shark', 'sheep', 'snail', 'snake', 'spider',
+			'squirrel', 'toucan', 'turkey', 'turtle', 'whale', 'zebra', 'apple',
+			'avocado', 'banana', 'bell pepper', 'blackberry', 'blueberry', 'cabbage',
+			'carrot', 'cherry', 'corn', 'cucumber', 'eggplant', 'garlic', 'grapes',
+			'kiwi', 'lemon', 'melon', 'mushroom', 'onion', 'orange', 'peach', 'pear',
+			'pineapple', 'plum', 'raspberry', 'strawberry', 'tomato', 'watermelon',
+			'zucchini', 'bathtub', 'bed', 'broom', 'computer', 'cup', 'door', 'fork',
+			'fridge', 'frying pan', 'glass', 'mixer', 'hanger', 'iron', 'knife',
+			'ladle', 'bulb', 'microwave', 'mirror', 'oven mitt', 'pan', 'plate',
+			'sink', 'sofa', 'spoon', 'table', 'tap', 'toaster', 'toilet', 'trash',
+			'wardrobe', 'window', 'acorn', 'autumn', 'bug', 'butterfly', 'cactus',
+			'cloud', 'feather', 'tree', 'flower', 'grass', 'holly', 'leaf',
+			'mountain', 'palm tree', 'rain', 'rainbow', 'reed', 'rose', 'spring',
+			'stone', 'summer', 'tree', 'volcano', 'winter', 'black', 'blue', 'brown',
+			'green', 'orange', 'pink', 'purple', 'red', 'white', 'yellow', 'bag',
+			'belt', 'boot', 'cap', 'dress', 'hat', 'heels', 'jeans', 'tie', 'panties',
+			'pants', 'shirt', 'shoes', 'sneakers', 'socks', 'suit', 'sweater', 'tee',
+			'shirt', 'thongs', 'arm', 'bandage', 'blood', 'brain', 'ear', 'embryo',
+			'eye', 'foot', 'hand', 'heart', 'kidney', 'knee', 'lungs', 'mouth',
+			'pills', 'syringe', 'skeleton', 'skull', 'teeth', 'thermometer', 'thumb',
+			'badminton', 'baseball', 'basketball', 'biking', 'bowling', 'boxing',
+			'cricket', 'golf', 'ice hockey', 'jogging', 'soccer', 'swimming',
+			'tennis', 'whistle', 'surfing', 'windsurfing', 'ant', 'bear', 'bee',
+			'bird', 'cat', 'chicken', 'cow', 'dog', 'elephant', 'fish', 'fox', 'frog',
+			'giraffe', 'horse', 'lamb', 'panda', 'penguin', 'pig', 'rabbit',
+			'seahorse', 'shark', 'sheep', 'snail', 'snake', 'spider', 'squirrel',
+			'toucan', 'turkey', 'turtle', 'whale', 'zebra', 'apple', 'avocado',
+			'banana', 'bell pepper', 'blackberry', 'blueberry', 'cabbage', 'carrot',
+			'cherry', 'corn', 'cucumber', 'eggplant', 'garlic', 'grapes', 'kiwi',
+			'lemon', 'melon', 'mushroom', 'onion', 'orange', 'peach', 'pear',
+			'pineapple', 'plum', 'raspberry', 'strawberry', 'tomato', 'watermelon',
+			'zucchini', 'bathtub', 'bed', 'broom', 'computer', 'cup', 'door', 'fork',
+			'fridge', 'frying pan', 'glass', 'mixer', 'hanger', 'iron', 'knife',
+			'ladle', 'bulb', 'microwave', 'mirror', 'oven mitt', 'pan', 'plate',
+			'sink', 'sofa', 'spoon', 'table', 'tap', 'toaster', 'toilet', 'trash can',
+			'wardrobe', 'window', 'badminton', 'baseball', 'basketball', 'biking',
+			'bowling', 'boxing', 'cricket', 'golf', 'hockey', 'jogging', 'soccer',
+			'swimming', 'tennis', 'whistle', 'surfing', 'windsurfing']; 
 
 const createNewWords = ()=>{
     let ranNum =Math.floor(Math.random() * sWords.length);
